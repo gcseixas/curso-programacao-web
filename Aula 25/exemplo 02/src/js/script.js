640efcffe203d26d793afa395aed8c980d8f6f08
Gabriel <gcseixas01@gmail.com>
2022-10-19 21:32:38 -0300

    commit

---
+++ Aula 25/exemplo 02/src/js/script.js
@@ -0,0 +1,12 @@
+
+
+function gerarCor(){
+    let r = Math.floor(Math.random() * 255);
+    let g = Math.floor(Math.random() * 255);
+    let b = Math.floor(Math.random() * 255);
+    
+    let box = document.getElementsByClassName('box')[0];
+    
+    box.style.backgroundColor = `rgb(${r},${g},${b} )`;
+
+}
